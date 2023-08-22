97cf503e27d127c01a8a50e7cfc0c5e13ed69811
sofiazxcruz <127714398+sofiazxcruz@users.noreply.github.com>
2023-08-21 22:55:48 -0600

    Add files via upload

---
+++ practica3POE/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace practica3POE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buscarboton = new System.Windows.Forms.Button();
+            this.buscarboton2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buscarboton
+            // 
+            this.buscarboton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buscarboton.Location = new System.Drawing.Point(78, 362);
+            this.buscarboton.Name = "buscarboton";
+            this.buscarboton.Size = new System.Drawing.Size(109, 39);
+            this.buscarboton.TabIndex = 0;
+            this.buscarboton.Text = "Cambiar";
+            this.buscarboton.UseVisualStyleBackColor = true;
+            this.buscarboton.Click += new System.EventHandler(this.buscarboton_Click);
+            // 
+            // buscarboton2
+            // 
+            this.buscarboton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buscarboton2.Location = new System.Drawing.Point(351, 361);
+            this.buscarboton2.Name = "buscarboton2";
+            this.buscarboton2.Size = new System.Drawing.Size(109, 39);
+            this.buscarboton2.TabIndex = 1;
+            this.buscarboton2.Text = "Cambiar";
+            this.buscarboton2.UseVisualStyleBackColor = true;
+            this.buscarboton2.Click += new System.EventHandler(this.buscarboton2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(247, 318);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(273, 24);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(264, 318);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(554, 24);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(264, 318);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(870, 450);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buscarboton2);
+            this.Controls.Add(this.buscarboton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buscarboton;
+        private System.Windows.Forms.Button buscarboton2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ practica3POE/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica3POE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buscarboton_Click(object sender, EventArgs e)
+        {
+            //utilizar la clase OpenFileDialog para ver la ventana
+
+            OpenFileDialog abrir = new OpenFileDialog();
+
+            //Filtrar para abrir imagenes jpg
+
+            abrir.Filter = "JPEG (*.JPG) |* JPG";
+
+            //verificar ventana y mostrarla
+            //showDialog abre una ventana, el if valida la respuesta
+            //en la ventana
+
+            if (abrir.ShowDialog() == DialogResult.OK)
+            {
+                //validaciones sobre ventana img
+                pictureBox1.Image = Image.FromFile(abrir.FileName);
+
+            }
+        }
+
+        private bool isImg = true;
+
+        private void buscarboton2_Click(object sender, EventArgs e)
+        {
+            if (isImg == true)
+            {
+                pictureBox2.Image = Image.FromFile(@"C:\Users\marth\source\repos\practica3POE\imagenes\imagennayeon.jpg");
+                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom; // ajusta la imagen en el picturebox
+                isImg = false;
+            }
+            else
+            {
+                pictureBox2.Image = Image.FromFile(@"C:\Users\marth\source\repos\practica3POE\imagenes\image5.jpg");
+                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+                isImg = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (isImg == true)
+            {
+                pictureBox3.Image = Image.FromFile(@"C:\Users\marth\source\repos\practica3POE\imagenes\imagennayeon.jpg");
+                pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+                isImg = false;
+            }
+            else
+            {
+                pictureBox3.Image = Image.FromFile(@"C:\Users\marth\source\repos\practica3POE\imagenes\image5.jpg");
+                pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+                isImg = true;
+            }
+        }
+    }
+}
+
